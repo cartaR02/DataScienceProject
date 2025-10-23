11f5ed134c16b24785be362f1604733b6e8bcba1
Carter Struck <carterstruck02@gmail.com>
2025-10-23 14:50:42 -0400

    working on getting features working

--- training.py
+++ training.py
@@ -1,4 +1,10 @@
+import numpy as np
+from collections import Counter
+
+
+
 file_name = "sequences_training.txt"
+
 
 feature_list = []
 label_list = []
@@ -10,8 +16,26 @@ try:
 
 			DNA,classification = line.split(',')
 			dnaLength = len(DNA)
-			
+			feature_list.append(DNA)
+			label_list.append(classification)
+
 			print(f"{dnaLength}: {classification}")
 
 except FileNotFoundError:
 	print(f"Error no file")
+
+counts = Counter(label_list)
+print(len(feature_list))
+print(len(label_list))
+for cls, count in counts.items():
+	print(f"{cls}: {count}")
+
+training_cycles = 8
+patterns = 1
+augmented_inputs = 3
+learning_constant = .1
+
+patterns = feature_list # TODO make a np array
+text_lables = ("DNA", "RNA", "nonDRNA", "DRNA")
+desired_output = ([1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1])
+
