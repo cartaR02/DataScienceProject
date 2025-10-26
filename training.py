bde6d07eec977c7aa15bcb5a107abfb96ee766e7
Carter Struck <carterstruck02@gmail.com>
2025-10-25 23:07:19 -0400

    trying not tio divide by zero

--- training.py
+++ training.py
@@ -8,7 +8,18 @@ file_name = "sequences_training.txt"
 
 feature_list = []
 label_list = []
+DNAsum = []
+RNAsum = []
+nonDRNAsum = []
+DRNAsum = []
+def getAverage(dnas_list):
+	length = len(dnas_list)
+	print(f"Length {length}")
+	total = 0
+	for d in dnas_list:
+		total += d
 
+	return total / length
 try: 
 	with open(file_name, 'r') as file:
 
@@ -18,8 +29,17 @@ try:
 			dnaLength = len(DNA)
 			feature_list.append(DNA)
 			label_list.append(classification)
+			match classification:
+				case "DNA":
+					print("DNA")
 
-			print(f"{dnaLength}: {classification}")
+					DNAsum.append(dnaLength)
+				case "RNA":
+					RNAsum.append(dnaLength)
+				case "nonDRNA":
+					nonDRNAsum.append(dnaLength)
+				case "DRNA":
+					DRNAsum.append(dnaLength)
 
 except FileNotFoundError:
 	print(f"Error no file")
@@ -27,9 +47,15 @@ except FileNotFoundError:
 counts = Counter(label_list)
 print(len(feature_list))
 print(len(label_list))
-for cls, count in counts.items():
-	print(f"{cls}: {count}")
 
+dna_average = getAverage(DNAsum)
+print(f"DNA Average: {dna_average}")
+rna_average = getAverage(RNAsum)
+print(f"RNA Average: {rna_average}")
+nondrna_average = getAverage(nonDRNAsum)
+print(f"nonDRNA Average: {nondrna_average}")
+drna_average = getAverage(DRNAsum)
+print(f"DRNA Average: {drna_average}")
 training_cycles = 8
 patterns = 1
 augmented_inputs = 3
