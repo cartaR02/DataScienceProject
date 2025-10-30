4e2546a9bddb1a674cce14d2324ed72592b18687
Carter Struck <carterstruck02@gmail.com>
2025-10-29 21:55:27 -0400

    better grousp

--- modal.py
+++ modal.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
 
 from datetime import datetime
 feature_list = []
@@ -39,9 +40,25 @@ try:
 except FileNotFoundError:
 	print(f"Error no file")
 
-# Used for typical scoring accuracy 
-NRangeModalOutput = []
-def runNRangeModal(nrange_lower, nrange_upper, feature_limit):
+def runTFIDDecisionTree():
+  
+    NRangeModalOutput = []
+    # start of main
+    NRangeModalOutput.append(["NRange_Upper", "NRange_Lower", "Feature_Limit", "Max_depth", "Score1", "Score2", "Score3", "Score4", "Score5", "Time Elapsed"])
+    nLower = 3
+    nUpper = 10
+    lowerFeature = 5000
+    upperFeature = 20000
+    print("--------------Decision Tree-----------------")
+    for n in range (nLower,nUpper):
+        for max_feat in range (lowerFeature, upperFeature, 5000):
+            for max_depth in range(10, 20):
+                NRangeModalOutput.append(runNRangeModal(nLower, n, max_feat, max_depth))
+    with open('NDecisionTree.csv', 'w', newline='') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerows(NRangeModalOutput)
+
+def runNRangeModal(nrange_lower, nrange_upper, feature_limit, depth):
     
     information_list = []
 
@@ -55,9 +72,60 @@ def runNRangeModal(nrange_lower, nrange_upper, feature_limit):
 
         # classifier learns to map nubmers to a label
         #('clf', LogisticRegression(max_iter=1000))
-        ('clf', DecisionTreeClassifier(random_state=0))
+        ('clf', DecisionTreeClassifier(random_state=0,
+                                       max_depth=depth))
     ])
 
+    print(f"NLower: {nrange_lower} NUpper: {nrange_upper} Max Feature: {feature_limit} Max Depth: {depth}")
+    information_list.append(nrange_lower)
+    information_list.append(nrange_upper)
+    information_list.append(feature_limit)
+    information_list.append(depth)
+
+    #getScores(text_classification_pipeline)
+    cross_validated_scores = cross_val_score(text_classification_pipeline, X_data, Y_data, cv=5)
+    print(cross_validated_scores)
+    for score in cross_validated_scores:
+        information_list.append(score)
+
+    end = datetime.now()
+    elapsed = end - start
+    elapsed_time = str(elapsed).split(".")[0]
+    print(f"Elapsed Time {elapsed_time}")
+    information_list.append(elapsed_time)
+    return information_list
+
+def runTFIDLogisticRegression():
+    LogisticOutput = []
+    nLower = 3
+    nUpper = 10
+    lowerFeature = 5000
+    upperFeature = 55000
+    
+    LogisticOutput.append(["NRange_Upper", "NRange_Lower", "Feature_Limit", "Score1", "Score2", "Score3", "Score4", "Score5", "Time Elapsed"])
+    print("--------------Logistic Regression-----------------")
+    for n in range (nLower, nUpper):
+        print("**********New nLower and nUpper************")
+        for max_feat in range (lowerFeature, upperFeature, 10000):
+            LogisticOutput.append(runLogisticModal(nLower, n, max_feat))
+    with open('NLogisticOutput.csv', 'w', newline='') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerows(LogisticOutput)
+
+def runLogisticModal(nrange_lower, nrange_upper, feature_limit):
+    
+    information_list = []
+
+    X_data = feature_list
+    Y_data = label_list
+    start = datetime.now()
+    # build the modal
+    text_classification_pipeline = Pipeline([
+        # Vectorize strings
+        ('tfidf',TfidfVectorizer(ngram_range=(nrange_lower, nrange_upper), analyzer='char',max_features=feature_limit)),
+
+        ('clf', LogisticRegression(max_iter=1000))
+    ])
     print(f"NLower: {nrange_lower} NUpper: {nrange_upper} Max Feature: {feature_limit}")
     information_list.append(nrange_lower)
     information_list.append(nrange_upper)
@@ -74,17 +142,72 @@ def runNRangeModal(nrange_lower, nrange_upper, feature_limit):
     elapsed_time = str(elapsed).split(".")[0]
     print(f"Elapsed Time {elapsed_time}")
     information_list.append(elapsed_time)
-    NRangeModalOutput.append(information_list)
-# start of main
-NRangeModalOutput.append(["NRange_Upper", "NRange_Lower", "Feature_Limit", "Score1", "Score2", "Score3", "Score4", "Score5", "Time Elapsed"])
-nLower = 6
-nUpper = 20
-lowerFeature = 50000
-upperFeature = 200000
-for n in range (nLower,nUpper):
-    for max_feat in range (lowerFeature, upperFeature, 5000):
-        runNRangeModal(nLower, n, max_feat)
+    return information_list
 
-with open('NRangeDecisionTree.csv', 'w', newline='') as outfile:
-    writer = csv.writer(outfile)
-    writer.writerows(NRangeModalOutput)
+
+def wrapperTFIDRandomForest():
+    RandomForestOutput = []
+    nLower = 3
+    nUpper = 10
+    lowerFeature = 5000
+    upperFeature = 55000
+    
+    RandomForestOutput.append(["NRange_Upper", "NRange_Lower", "Feature_Limit", "Score1", "Score2", "Score3", "Score4", "Score5", "Time Elapsed"])
+    print("--------------Random Tree-----------------")
+    for n in range (nLower, nUpper):
+        print("**********New nLower and nUpper************")
+        for max_feat in range (lowerFeature, upperFeature, 10000):
+            RandomForestOutput.append(runRandomForestModal(nLower, n, max_feat))
+    with open('NRandomForest.csv', 'w', newline='') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerows(RandomForestOutput)
+
+def runRandomForestModal(nrange_lower, nrange_upper, feature_limit):
+    
+    information_list = []
+
+    X_data = feature_list
+    Y_data = label_list
+    start = datetime.now()
+    # build the modal
+    text_classification_pipeline = Pipeline([
+        # Vectorize strings
+        ('tfidf',TfidfVectorizer(ngram_range=(nrange_lower, nrange_upper), analyzer='char',max_features=feature_limit)),
+
+        ('clf', RandomForestClassifier(
+                                       n_estimators=300,
+                                       max_depth=None,
+                                       min_samples_split=2,
+                                       min_samples_leaf=1,
+                                       max_features='sqrt',
+                                       n_jobs=1,
+                                       random_state=42))
+    ])
+    print(f"NLower: {nrange_lower} NUpper: {nrange_upper} Max Feature: {feature_limit}")
+    information_list.append(nrange_lower)
+    information_list.append(nrange_upper)
+    information_list.append(feature_limit)
+
+    #getScores(text_classification_pipeline)
+    cross_validated_scores = cross_val_score(text_classification_pipeline, X_data, Y_data, cv=5)
+    print(cross_validated_scores)
+    for score in cross_validated_scores:
+        information_list.append(score)
+
+    end = datetime.now()
+    elapsed = end - start
+    elapsed_time = str(elapsed).split(".")[0]
+    print(f"Elapsed Time {elapsed_time}")
+    information_list.append(elapsed_time)
+    return information_list
+
+
+
+def main():
+
+    runTFIDDecisionTree()
+    #runTFIDLogisticRegression()
+    #wrapperTFIDRandomForest()
+
+if __name__ == "__main__":
+    main()
